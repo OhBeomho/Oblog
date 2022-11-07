ac75883d46381550feb9c47c7585a1c52dd6c5c1
OhBeomho <tyler0322@naver.com>
2022-11-07 21:15:01 +0900

    sort

--- server.js
+++ server.js
@@ -266,7 +266,7 @@ app.post("/read", (req, res) => {
 app.post("/blogs", (req, res) => {
 	const { offset, count } = req.body;
 
-	db.query(`SELECT * FROM blog ORDER BY writedate DESC LIMIT ${count} OFFSET ${offset}`, (err, result) => {
+	db.query(`SELECT * FROM blog ORDER BY writedate ASC LIMIT ${count} OFFSET ${offset}`, (err, result) => {
 		if (err) {
 			console.error(err.message);
 			res.sendStatus(500);
